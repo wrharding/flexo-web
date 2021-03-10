ae43802001e0f362f09f9e126dc514fe70a1e38f
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2021-03-09 21:17:41 -0500

    fixed api calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,21 @@
-
-
-export const fetchData = (path, options) =>{
-   return fetch(`/${path}`, options)
-        .then(response => response.json())
-        .then(data => {
-            if(Array.isArray(data)){
-            return data.map((item) => {
-                return {value: item.ID, label: item.Name}
-            })
-            }
-                console.log("GET Res: ", data);
-                return data
-
+export const fetchData = (path, options) => {
+  return fetch(`${process.env.BACKEND_URL}/${path}`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      if (Array.isArray(data)) {
+        return data.map((item) => {
+          return { value: item.ID, label: item.Name };
         });
-}
+      }
+      return data;
+    });
+};
 
 export const postData = async (path, options) => {
-    console.log("Post Options: ", options);
-    return await fetch(`/${path}`, options)
-        .then(response => response.json())
-        .then(data => {
-           console.log("Post res: ", data);
-            });
-        };
+  return await fetch(`${process.env.BACKEND_URL}/${path}`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Post res: ', data);
+      return data;
+    });
+};
